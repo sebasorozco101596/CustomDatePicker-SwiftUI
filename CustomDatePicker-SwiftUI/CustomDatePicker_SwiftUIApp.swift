1655ff8f6df1d266514d80234c7b698c2a069e0a
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-02 23:32:14 -0500

    Initial Commit

---
+++ CustomDatePicker-SwiftUI/CustomDatePicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDatePicker_SwiftUIApp.swift
+//  CustomDatePicker-SwiftUI
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomDatePicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
